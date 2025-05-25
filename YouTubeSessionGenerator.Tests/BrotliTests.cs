f3414c5f482f42922c773b9d75c0dd08622079d8
IcySnex <lao43919@gmail.com>
2025-05-25 21:53:17 +0200

    Added Brotli compression utils

---
+++ YouTubeSessionGenerator.Tests/BrotliTests.cs
@@ -0,0 +1,56 @@
+﻿using YouTubeSessionGenerator.Utils;
+
+namespace YouTubeSessionGenerator.Tests;
+
+public class BrotliTests
+{
+    [Test]
+    public void Should_compress_using_brotli()
+    {
+        // Arrange
+        string input = "This is a test string to compress using Brotli compression.";
+
+        // Act
+        string? result = null;
+
+        Assert.DoesNotThrow(() =>
+        {
+            result = Brotli.Compress(input);
+        });
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo("GzoAcBypU587dC0U9ianQOjSJY0NNuDAoZeURhw24JgTCTBdbr2oSLmS7LiONZ97EAI="));
+        });
+
+        // Output
+        TestContext.Out.WriteLine("Result: {0}", result);
+    }
+    
+
+    [Test]
+    public void Should_decompress_using_brotli()
+    {
+        // Arrange
+        string input = "GzoAcBypU587dC0U9ianQOjSJY0NNuDAoZeURhw24JgTCTBdbr2oSLmS7LiONZ97EAI=";
+
+        // Act
+        string? result = null;
+
+        Assert.DoesNotThrow(() =>
+        {
+            result = Brotli.Decompress(input);
+        });
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo("This is a test string to compress using Brotli compression."));
+        });
+
+        // Output
+        TestContext.Out.WriteLine("Result: {0}", result);
+    }
+
+}
---
+++ YouTubeSessionGenerator/Utils/Brotli.cs
@@ -0,0 +1,47 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace YouTubeSessionGenerator.Utils;
+
+/// <summary>
+/// Contains methods to compress and decompress strings using Brotli compression.
+/// </summary>
+internal static class Brotli
+{
+    /// <summary>
+    /// Compresses a string using Brotli compression.
+    /// </summary>
+    /// <param name="input">The input string.</param>
+    /// <returns>The result as a Base64 encoded string.</returns>
+    public static string Compress(
+        string input)
+    {
+        byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+        using MemoryStream outputStream = new();
+        using (BrotliStream brotliStream = new(outputStream, CompressionLevel.SmallestSize))
+            brotliStream.Write(inputBytes, 0, inputBytes.Length);
+
+        byte[] compressedBytes = outputStream.ToArray();
+        return Convert.ToBase64String(compressedBytes);
+    }
+
+    /// <summary>
+    /// Decompresses a Brotli compressed string.
+    /// </summary>
+    /// <param name="input">The input stromg as a Base64 encoded string.</param>
+    /// <returns>The result.</returns>
+    public static string Decompress(
+        string input)
+    {
+        byte[] inputBytes = Convert.FromBase64String(input);
+        using MemoryStream inputStream = new(inputBytes);
+
+        using BrotliStream brotliStream = new(inputStream, CompressionMode.Decompress);
+        using MemoryStream outputStream = new();
+        brotliStream.CopyTo(outputStream);
+
+        byte[] decompressedBytes = outputStream.ToArray();
+        return Encoding.UTF8.GetString(decompressedBytes);
+    }
+}
