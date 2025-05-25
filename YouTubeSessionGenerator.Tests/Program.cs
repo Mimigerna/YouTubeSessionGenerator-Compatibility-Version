e8a4a340aa8f37f6352e02ee1b47ab0a0139867c
IcySnex <lao43919@gmail.com>
2025-05-25 13:48:13 +0200

    Project setup

---
+++ YouTubeSessionGenerator.Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace YouTubeSessionGenerator.Tests;
+
+public class Program
+{
+    [Test]
+    public async Task MainAsync()
+    {
+        // Getting Started
+        ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = factory.CreateLogger<Program>();
+
+
+    }
+}
